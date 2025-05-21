ef58e07d9068bb66193f343e1115d6a3d078a05c
Antonio Benitez <mrsasuu@gmail.com>
2025-05-21 22:30:53 +0200

    Enables Module Federation
    Configures the application to use Module Federation,
    allowing it to share code and dependencies with other
    React Native applications.
    
    Defines shared dependencies to ensure version compatibility
    and reduces bundle size. Also configures a standalone
    start script, to serve the app independently.

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -2,10 +2,13 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import * as Repack from '@callstack/repack';
 import rspack from '@rspack/core';
+import getSharedDependencies from './sharedDeps.js';
 import { ReanimatedPlugin } from '@callstack/repack-plugin-reanimated';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const STANDALONE = Boolean(process.env.STANDALONE);
 
 /**
  * Rspack configuration enhanced with Re.Pack defaults for React Native.
@@ -14,23 +17,41 @@ const __dirname = path.dirname(__filename);
  * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
  */
 
-export default {
-  context: __dirname,
-  entry: './index.js',
-  resolve: {
-    ...Repack.getResolveOptions(),
-  },
-  module: {
-    rules: [
-      ...Repack.getJsTransformRules(),
-      ...Repack.getAssetTransformRules(),
+export default env => {
+  const { mode } = env;
+
+  return {
+    mode,
+    context: __dirname,
+    entry: './index.js',
+    resolve: {
+      ...Repack.getResolveOptions(),
+    },
+    output: {
+      uniqueName: 'adopta-app',
+    },
+    module: {
+      rules: [
+        ...Repack.getJsTransformRules(),
+        ...Repack.getAssetTransformRules({ inline: true }),
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin(),
+      new ReanimatedPlugin(),
+      new Repack.plugins.ModuleFederationPluginV2({
+        name: 'adoptaApp',
+        filename: 'adoptaApp.container.js.bundle',
+        dts: false,
+        exposes: {
+          './App': './src/AppContent',
+        },
+        shared: getSharedDependencies({ eager: STANDALONE }),
+      }),
+      // silence missing @react-native-masked-view optionally required by @react-navigation/elements
+      new rspack.IgnorePlugin({
+        resourceRegExp: /^@react-native-masked-view/,
+      }),
     ],
-  },
-  plugins: [
-    new Repack.RepackPlugin(),
-    new ReanimatedPlugin(),
-    // silence missing @react-native-masked-view optionally required by @react-navigation/elements
-    new rspack.IgnorePlugin({
-      resourceRegExp: /^@react-native-masked-view/,
-    })],
+  };
 };
